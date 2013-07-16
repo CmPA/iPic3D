CmPA/iPic3D

commit 6c90362934902521a9a9a2fcf4e47a0a8df86090

Restored implementation of TimeTasks (issue #17).  Some communication time for accumulating moments was being reckoned as calculation time.

utility/TimeTasks.cpp
@@ -1,0 +1,124 @@
+
+#include <mpi.h>
+#include <stdarg.h>
+#include "TimeTasks.h"
+#include "asserts.h"
+#include "MPIdata.h" // for get_rank
+
+/** implementation of declarations in utility/TimeTasks.h **/
+
+TimeTasks timeTasks;
+
+void TimeTasks::resetCycle()
+{
+  for(int e=0;e<LAST;e++)
+  {
+    //compute[e]=0.;
+    start_times[e]=0.;
+    task_duration[e]=0.;
+    communicate[e]=0.;
+  }
+  active_task=NONE;
+  active_mode=COMPUTATION;
+  t_start_communicate = 0.;
+}
+void TimeTasks::start(int taskid)
+{
+  assert_eq(active_task+1,taskid);
+  active_task = taskid;
+  double now = MPI_Wtime();
+  start_times[active_task] = now;
+}
+void TimeTasks::end(int taskid)
+{
+  assert_eq(taskid,active_task);
+  double now = MPI_Wtime();
+  task_duration[active_task] = now - start_times[active_task];
+  compute[active_task] = task_duration[active_task]-communicate[active_task];
+}
+void TimeTasks::start_communicate()
+{
+  if(!active_task) return;
+  assert_eq(active_mode,COMPUTATION);
+  t_start_communicate = MPI_Wtime();
+  active_mode=COMMUNICATION;
+}
+void TimeTasks::addto_communicate()
+{
+  if(!active_task) return;
+  assert_eq(active_mode,COMMUNICATION);
+  assert_ne(t_start_communicate,0.);
+  communicate[active_task] += MPI_Wtime()-t_start_communicate;
+  t_start_communicate = 0.;
+  active_mode=COMPUTATION;
+}
+#define TIMING_PREFIX "| "
+void TimeTasks::print_cycle_times()
+{
+  // we could report average for all processes
+  if(!MPIdata::get_rank())
+  {
+    fflush(stdout);
+    fprintf(stdout,"=== times for cycle for rank %d) === \n",
+      MPIdata::get_rank());
+    fprintf(stdout, TIMING_PREFIX
+      "moms flds pcls Bfld cycl\n");
+    fprintf(stdout, TIMING_PREFIX
+      "%4.2f "
+      "%4.2f "
+      "%4.2f "
+      "%4.2f "
+      "%4.2f (total time)\n",
+      get_time(TimeTasks::MOMENTS),
+      get_time(TimeTasks::FIELDS),
+      get_time(TimeTasks::PARTICLES),
+      get_time(TimeTasks::BFIELD),
+      get_time()
+      );
+    fprintf(stdout, TIMING_PREFIX
+      "%4.2f "
+      "%4.2f "
+      "%4.2f "
+      "%4.2f "
+      "%4.2f (communication)\n",
+      get_communicate(TimeTasks::MOMENTS),
+      get_communicate(TimeTasks::FIELDS),
+      get_communicate(TimeTasks::PARTICLES),
+      get_communicate(TimeTasks::BFIELD),
+      get_communicate()
+      );
+    fprintf(stdout, TIMING_PREFIX
+      "%4.2f "
+      "%4.2f "
+      "%4.2f "
+      "%4.2f "
+      "%4.2f (computation)\n",
+      get_compute(TimeTasks::MOMENTS),
+      get_compute(TimeTasks::FIELDS),
+      get_compute(TimeTasks::PARTICLES),
+      get_compute(TimeTasks::BFIELD),
+      get_compute()
+      );
+    //fprintf(stdout, TIMING_PREFIX
+    //  "MOMS comm  FLDS comm  PCLS comm  CYCL comm\n");
+    //fprintf(stdout, TIMING_PREFIX
+    //  "%4.2f "
+    //  "%4.2f  "
+    //  "%4.2f "
+    //  "%4.2f  "
+    //  "%4.2f "
+    //  "%4.2f  "
+    //  "%4.2f "
+    //  "%4.2f\n",
+    //  get_time(TimeTasks::MOMENTS),
+    //  get_communicate(TimeTasks::MOMENTS),
+    //  get_time(TimeTasks::FIELDS),
+    //  get_communicate(TimeTasks::FIELDS),
+    //  get_time(TimeTasks::PARTICLES),
+    //  get_communicate(TimeTasks::PARTICLES),
+    //  get_time(),
+    //  get_communicate()
+    //  );
+    fflush(stdout);
+  }
+}

communication/ComNodes3D.cpp
@@ -1,10 +1,11 @@
 
 #include "ComNodes3D.h"
+#include "TimeTasks.h"
 
 /** communicate ghost cells (FOR NODES) */
 void communicateNode(int nx, int ny, int nz, double ***vector, VirtualTopology3D * vct) {
+  timeTasks.start_communicate();
 
-  // timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -101,12 +102,12 @@ void communicateNode(int nx, int ny, int nz, double ***vector, VirtualTopology3D
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }
 /** communicate ghost cells (FOR NODES) */
 void communicateNodeBC(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -206,12 +207,12 @@ void communicateNodeBC(int nx, int ny, int nz, double ***vector, int bcFaceXrigh
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }
 /** communicate ghost cells (FOR NODES) with particles BC*/
 void communicateNodeBC_P(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -311,14 +312,14 @@ void communicateNodeBC_P(int nx, int ny, int nz, double ***vector, int bcFaceXri
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }
 
 /** SPECIES: communicate ghost cells */
 void communicateNode(int nx, int ny, int nz, double ****vector, int ns, VirtualTopology3D * vct) {
 
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -414,15 +415,15 @@ void communicateNode(int nx, int ny, int nz, double ****vector, int ns, VirtualT
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }                               // 
 
 // PARTICLES
 /** SPECIES: communicate ghost cells */
 void communicateNode_P(int nx, int ny, int nz, double ****vector, int ns, VirtualTopology3D * vct) {
+  timeTasks.start_communicate();
 
-  // timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -518,15 +519,15 @@ void communicateNode_P(int nx, int ny, int nz, double ****vector, int ns, Virtua
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }
 
 // 
 /** communicate ghost cells (FOR CENTERS) */
 void communicateCenter(int nx, int ny, int nz, double ***vector, VirtualTopology3D * vct) {
 
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -621,12 +622,12 @@ void communicateCenter(int nx, int ny, int nz, double ***vector, VirtualTopology
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }
 /** communicate ghost cells (FOR CENTERS) with BOX stencil*/
 void communicateCenterBoxStencilBC(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -653,12 +654,12 @@ void communicateCenterBoxStencilBC(int nx, int ny, int nz, double ***vector, int
   delete[]ghostYleftFace;
   delete[]ghostZrightFace;
   delete[]ghostZleftFace;
-  // timeTasks.addto_communicate();
+  timeTasks.addto_communicate();
 }
 // particles
 /** communicate ghost cells (FOR CENTERS) with BOX stencil*/
 void communicateCenterBoxStencilBC_P(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -685,14 +686,14 @@ void communicateCenterBoxStencilBC_P(int nx, int ny, int nz, double ***vector, i
   delete[]ghostYleftFace;
   delete[]ghostZrightFace;
   delete[]ghostZleftFace;
-  // timeTasks.addto_communicate();
+  timeTasks.addto_communicate();
 }
 
 // 
 
 
 void communicateNodeBoxStencilBC(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -719,11 +720,11 @@ void communicateNodeBoxStencilBC(int nx, int ny, int nz, double ***vector, int b
   delete[]ghostYleftFace;
   delete[]ghostZrightFace;
   delete[]ghostZleftFace;
-  // timeTasks.addto_communicate();
+  timeTasks.addto_communicate();
 }
 
 void communicateNodeBoxStencilBC_P(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
-  // timeTasks.start_communicate();
+  timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -750,15 +751,15 @@ void communicateNodeBoxStencilBC_P(int nx, int ny, int nz, double ***vector, int
   delete[]ghostYleftFace;
   delete[]ghostZrightFace;
   delete[]ghostZleftFace;
-  // timeTasks.addto_communicate();
+  timeTasks.addto_communicate();
 }
 
 
 
 /** SPECIES: communicate ghost cells */
 void communicateCenter(int nx, int ny, int nz, double ****vector, int ns, VirtualTopology3D * vct) {
+  timeTasks.start_communicate();
 
-  // timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -852,13 +853,13 @@ void communicateCenter(int nx, int ny, int nz, double ****vector, int ns, Virtua
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }
 // /////////// communication + BC ////////////////////////////
 void communicateCenterBC(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
+  timeTasks.start_communicate();
 
-  // timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -955,13 +956,13 @@ void communicateCenterBC(int nx, int ny, int nz, double ***vector, int bcFaceXri
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }
 // /////////// communication + BC ////////////////////////////
 void communicateCenterBC_P(int nx, int ny, int nz, double ***vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, VirtualTopology3D * vct) {
+  timeTasks.start_communicate();
 
-  // timeTasks.start_communicate();
   // allocate 6 ghost cell Faces
   double *ghostXrightFace = new double[(ny - 2) * (nz - 2)];
   double *ghostXleftFace = new double[(ny - 2) * (nz - 2)];
@@ -1058,6 +1059,6 @@ void communicateCenterBC_P(int nx, int ny, int nz, double ***vector, int bcFaceX
   delete[]ghostXrightYrightZsameEdge;
   delete[]ghostXleftYleftZsameEdge;
   delete[]ghostXleftYrightZsameEdge;
-  // timeTasks.addto_communicate();
 
+  timeTasks.addto_communicate();
 }

fields/EMfields3D.cpp
@@ -1,6 +1,7 @@
 
 #include <mpi.h>
 #include "EMfields3D.h"
+#include "TimeTasks.h"
 
 /*! constructor */
 EMfields3D::EMfields3D(Collective * col, Grid * grid) {
@@ -1072,6 +1073,8 @@ void EMfields3D::interpDensitiesN2C(VirtualTopology3D * vct, Grid * grid) {
 /*! communicate ghost for grid -> Particles interpolation */
 void EMfields3D::communicateGhostP2G(int ns, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, VirtualTopology3D * vct) {
   // interpolate adding common nodes among processors
+  timeTasks.start_communicate();
+
   communicateInterp(nxn, nyn, nzn, ns, rhons, 0, 0, 0, 0, 0, 0, vct);
   communicateInterp(nxn, nyn, nzn, ns, Jxs, 0, 0, 0, 0, 0, 0, vct);
   communicateInterp(nxn, nyn, nzn, ns, Jys, 0, 0, 0, 0, 0, 0, vct);
@@ -1085,6 +1088,7 @@ void EMfields3D::communicateGhostP2G(int ns, int bcFaceXright, int bcFaceXleft, 
   // calculate the correct densities on the boundaries
   adjustNonPeriodicDensities(ns, vct);
   // put the correct values on ghost cells
+  timeTasks.addto_communicate();
 
   communicateNode_P(nxn, nyn, nzn, rhons, ns, vct);
   communicateNode_P(nxn, nyn, nzn, Jxs, ns, vct);
@@ -1096,7 +1100,6 @@ void EMfields3D::communicateGhostP2G(int ns, int bcFaceXright, int bcFaceXleft, 
   communicateNode_P(nxn, nyn, nzn, pYYsn, ns, vct);
   communicateNode_P(nxn, nyn, nzn, pYZsn, ns, vct);
   communicateNode_P(nxn, nyn, nzn, pZZsn, ns, vct);
-
 }
 
 

iPic3D.cpp
@@ -2,7 +2,6 @@
 #include <mpi.h>
 #include <iomanip>
 #include "iPic3D.h"
-#include "debug.h"
 
 using namespace iPic3D;
 
@@ -12,7 +11,6 @@ int main(int argc, char **argv) {
   bool b_err = false;
 
   MPIdata::init(&argc, &argv);
-  //dprintf("MPI has been initialized.");
   KCode.Init(argc, argv);
 
   for (int i = KCode.FirstCycle(); i < KCode.LastCycle(); i++) {

include/ComNodes3D.h
@@ -11,9 +11,9 @@ developers           : Stefano Markidis, Giovanni Lapenta
 #define ComNodes_H
 
 #include "ComBasic3D.h"
-#include "TimeTasks.h"
+//#include "TimeTasks.h"
 
-extern TimeTasks timeTasks;
+//extern TimeTasks timeTasks;
 
 // boundary condition for fields
 #include "BcFields3D.h"

include/EMfields3D.h
@@ -21,7 +21,7 @@
 #include "Collective.h"
 #include "ComNodes3D.h"
 #include "ComInterpNodes3D.h"
-#include "TimeTasks.h"
+//#include "TimeTasks.h"
 #include "asserts.h"
 #include "BCStructure.h"
 

include/Particles3D.h
@@ -8,7 +8,7 @@ developers: Stefano Markidis, Enrico Camporeale, Giovanni Lapenta, David Burgess
 #define Part2D_H
 
 #include "Particles3Dcomm.h"
-#include "TimeTasks.h"
+//#include "TimeTasks.h"
 
 /**
  * 

main/iPic3Dlib.cpp
@@ -1,5 +1,6 @@
 
 #include "iPic3D.h"
+#include "TimeTasks.h"
 
 using namespace iPic3D;
 MPIdata* iPic3D::c_Solver::mpi=0;
@@ -164,15 +165,17 @@ int c_Solver::Init(int argc, char **argv) {
 
 void c_Solver::CalculateField() {
 
-  // timeTasks.resetCycle();
+  timeTasks.resetCycle();
   // interpolation
-  // timeTasks.start(TimeTasks::MOMENTS);
+  timeTasks.start(TimeTasks::MOMENTS);
 
   EMf->updateInfoFields(grid,vct,col);
   EMf->setZeroDensities();                  // set to zero the densities
 
   for (int i = 0; i < ns; i++)
+  {
     part[i].interpP2G(EMf, grid, vct);      // interpolate Particles to Grid(Nodes)
+  }
 
   EMf->sumOverSpecies(vct);                 // sum all over the species
 
@@ -188,12 +191,12 @@ void c_Solver::CalculateField() {
   EMf->interpDensitiesN2C(vct, grid);       // calculate densities on centers from nodes
   EMf->calculateHatFunctions(grid, vct);    // calculate the hat quantities for the implicit method
   MPI_Barrier(MPI_COMM_WORLD);
-  // timeTasks.end(TimeTasks::MOMENTS);
+  timeTasks.end(TimeTasks::MOMENTS);
 
   // MAXWELL'S SOLVER
-  // timeTasks.start(TimeTasks::FIELDS);
+  timeTasks.start(TimeTasks::FIELDS);
   EMf->calculateE(grid, vct, col);               // calculate the E field
-  // timeTasks.end(TimeTasks::FIELDS);
+  timeTasks.end(TimeTasks::FIELDS);
 
 }
 
@@ -203,13 +206,13 @@ bool c_Solver::ParticlesMover() {
   /*  Particle mover */
   /*  -------------- */
 
-  // timeTasks.start(TimeTasks::PARTICLES);
+  timeTasks.start(TimeTasks::PARTICLES);
   for (int i = 0; i < ns; i++)  // move each species
   {
     // #pragma omp task inout(part[i]) in(grid) target_device(booster)
     mem_avail = part[i].mover_PC(grid, vct, EMf); // use the Predictor Corrector scheme 
   }
-  // timeTasks.end(TimeTasks::PARTICLES);
+  timeTasks.end(TimeTasks::PARTICLES);
 
   if (mem_avail < 0) {          // not enough memory space allocated for particles: stop the simulation
     if (myrank == 0) {
@@ -252,12 +255,12 @@ bool c_Solver::ParticlesMover() {
   /* This step must be taken out of here! */
   /* --------------------- */
 
-  // timeTasks.start(TimeTasks::BFIELD);
+  timeTasks.start(TimeTasks::BFIELD);
   EMf->calculateB(grid, vct, col);   // calculate the B field
-  // timeTasks.end(TimeTasks::BFIELD);
+  timeTasks.end(TimeTasks::BFIELD);
 
   // print out total time for all tasks
-  // timeTasks.print_cycle_times();
+  timeTasks.print_cycle_times();
   return (false);
 
 }

commit 23f510cba5e2b162285efe7ffb51293135b8b9bd

fixed typo in TimeTask output introduced in commit 6c903629

utility/TimeTasks.cpp
@@ -59,7 +59,7 @@ void TimeTasks::print_cycle_times()
   if(!MPIdata::get_rank())
   {
     fflush(stdout);
-    fprintf(stdout,"=== times for cycle for rank %d) === \n",
+    fprintf(stdout,"=== times for cycle for rank %d === \n",
       MPIdata::get_rank());
     fprintf(stdout, TIMING_PREFIX
       "moms flds pcls Bfld cycl\n");