CmPA/iPic3D

commit e1dba2f4abd5839ee618b448dc31ed7bb3d94843

implemented mover_PC_AoS (array of structs particles)

include/Grid3DCU.h
@@ -170,9 +170,9 @@ public: // accessors (inline)
   // coordinate accessors
   //
   // calculated equivalents (preferred for accelerator?):
-  //const double &calcXN(int X) { return xStart+(X-1)*dx;}
-  //const double &calcYN(int Y) { return yStart+(Y-1)*dy;}
-  //const double &calcZN(int Z) { return zStart+(Z-1)*dz;}
+  const double calcXN(int X) { return xStart+(X-1)*dx;}
+  const double calcYN(int Y) { return yStart+(Y-1)*dy;}
+  const double calcZN(int Z) { return zStart+(Z-1)*dz;}
   const pfloat &get_pfloat_XN(int X) { return pfloat_node_xcoord[X];}
   const pfloat &get_pfloat_YN(int Y) { return pfloat_node_ycoord[Y];}
   const pfloat &get_pfloat_ZN(int Z) { return pfloat_node_zcoord[Z];}

include/ipicmath.h
@@ -1,0 +1,47 @@
+#ifndef _ipicmath_h_
+#define _ipicmath_h_
+
+// valid if roundup power is representable.
+inline int
+pow2roundup (int x)
+{
+    assert(x>=0);
+    //if (x < 0)
+    //    return 0;
+    --x;
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+    return x+1;
+}
+
+// does not work if highest non-sign bit is set
+inline int
+pow2rounddown (int x)
+{
+    assert(x>=0);
+    //if (x < 0)
+    //    return 0;
+
+    // set all bits below highest bit
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+    // set the bit higher than the highest bit
+    x++;
+    // shift it down and return it
+    return (x >> 1);
+}
+
+// round n up to next multiple of m
+inline int roundup_to_multiple(int n, int m)
+{
+  //return ((n-1)/m+1)*m;
+  return (n+m-1)/m*m;
+}
+
+#endif

particles/Particles3Dcomm.cpp
@@ -23,6 +23,7 @@ developers: Stefano Markidis, Giovanni Lapenta.
 #include "Field.h"
 #include "MPIdata.h"
 #include "ompdefs.h"
+#include "ipicmath.h"
 
 #include "Particles3Dcomm.h"
 #include "Parameters.h"
@@ -55,6 +56,7 @@ Particles3Dcomm::Particles3Dcomm(){
 }
 /** deallocate particles */
 Particles3Dcomm::~Particles3Dcomm() {
+  delete[]pcls;
   delete[]x;
   delete[]y;
   delete[]z;
@@ -100,6 +102,8 @@ void Particles3Dcomm::allocate(int species, CollectiveIO * col, VirtualTopology3
   nop = col->getNp(species) / (vct->getNprocs());
   np_tot = col->getNp(species);
   npmax = col->getNpMax(species) / (vct->getNprocs());
+  // ensure that npmax is a multiple of AoS_PCLS_AT_A_TIME
+  npmax = roundup_to_multiple(npmax,AoS_PCLS_AT_A_TIME);
   qom = col->getQOM(species);
   uth = col->getUth(species);
   vth = col->getVth(species);
@@ -178,6 +182,21 @@ void Particles3Dcomm::allocate(int species, CollectiveIO * col, VirtualTopology3
   // //////////////////////////////////////////////////////////////
   // ////////////// ALLOCATE ARRAYS /////////////////////////
   // //////////////////////////////////////////////////////////////
+  //
+  // AoS particle representation
+  //
+  // intel new allocates with 64-byte alignment
+  // since particles are 64 bytes wide, every particle
+  // is aligned.
+  pcls = new SpeciesParticle[npmax];
+  particleType = ParticleType::SoA;
+  #ifdef __INTEL_COMPILER
+    assert_eq(sizeof(Particle),64);
+    ALIGNED(pcls);
+  #endif
+  //
+  // SoA particle representation
+  //
   // positions
   x = new double[npmax];
   y = new double[npmax];
@@ -1214,4 +1233,54 @@ void Particles3Dcomm::sort_particles_serial(
 //}
 //#endif
 
+void Particles3Dcomm::copyParticlesToSoA()
+{
+  dprintf("copying to struct of arrays");
+  #pragma omp for
+  for(int pidx=0; pidx<nop; pidx++)
+  {
+    const SpeciesParticle& pcl = pcls[pidx];
+    if(ParticleID) ParticleID[pidx] = pcl.get_ID();
+    x[pidx] = pcl.get_x(0);
+    y[pidx] = pcl.get_x(1);
+    z[pidx] = pcl.get_x(2);
+    u[pidx] = pcl.get_u(0);
+    v[pidx] = pcl.get_u(1);
+    w[pidx] = pcl.get_u(2);
+    q[pidx] = pcl.get_q();
+  }
+}
+
+void Particles3Dcomm::copyParticlesToAoS()
+{
+  #pragma omp for
+  dprintf("copying to array of structs");
+  for(int pidx=0; pidx<nop; pidx++)
+  {
+    pcls[pidx].set( ParticleID ? ParticleID[pidx] : 0,
+      x[pidx],y[pidx],z[pidx],
+      u[pidx],v[pidx],w[pidx],
+      q[pidx]);
+  }
+}
+
+void Particles3Dcomm::convertParticlesToAoS()
+{
+  if(particleType!=ParticleType::AoS)
+  {
+    assert_eq(particleType,ParticleType::SoA);
+    copyParticlesToAoS();
+    particleType = ParticleType::AoS;
+  }
+}
+
+void Particles3Dcomm::convertParticlesToSoA()
+{
+  if(particleType != ParticleType::SoA)
+  {
+    assert_eq(particleType,ParticleType::AoS);
+    copyParticlesToSoA();
+    particleType = ParticleType::SoA;
+  }
+}
 

fields/EMfields3D.cpp
@@ -395,6 +395,7 @@ void EMfields3D::sumMoments(const Particles3Dcomm* part, Grid * grid, VirtualTop
   for (int i = 0; i < ns; i++)
   {
     const Particles3Dcomm& pcls = part[i];
+    assert_eq(pcls.get_particleType(), ParticleType::AoS);
     const int is = pcls.get_ns();
     assert_eq(i,is);
 
@@ -846,6 +847,7 @@ void EMfields3D::sumMoments_vectorized(
   for (int species_idx = 0; species_idx < ns; species_idx++)
   {
     const Particles3Dcomm& pcls = part[species_idx];
+    assert_eq(pcls.get_particleType(), ParticleType::SoA);
     const int is = pcls.get_ns();
     assert_eq(species_idx,is);
 

iPic3D.cpp
@@ -34,6 +34,7 @@ int main(int argc, char **argv) {
       i = KCode.LastCycle() + 1;
     }
 
+    KCode.convertParticlesToSoA();
     KCode.WriteOutput(i);
     KCode.WriteConserved(i);
     KCode.WriteRestart(i);

include/Alloc.h
@@ -74,6 +74,12 @@
     #define AlignedFree(S) (delete[] S)
     #define AlignedAlloc(T, NUM) (new T[NUM]) 
 #endif
+inline bool is_aligned(void *p, int N)
+{
+    return (unsigned long)p % N == 0;
+}
+#define assert_aligned(X, N) assert(is_aligned(X, N));
+
 
 // Compile with -DCHECK_BOUNDS to turn on bounds checking.
 //#define CHECK_BOUNDS

include/PSKOutput.h
@@ -15,7 +15,7 @@ developers: D. Burgess, June/July 2006
 
 #include "errors.h"
 #include "PSKException.h"
-#include "Particles.h"
+#include "Particles3Dcomm.h"
 #include "Field.h"
 #include "Grid.h"
 #include "Collective.h"

include/Particle.h
@@ -1,0 +1,115 @@
+#ifndef _Particle_
+#define _Particle_
+
+// Depends on width of vector unit;
+// need to be known at compile time.
+//
+#define AoS_PCLS_AT_A_TIME 2
+
+namespace ParticleType
+{
+  enum Type
+  {
+    AoS = 0,
+    SoA
+  };
+}
+
+// intended to occupy 64 bytes
+//
+// particle for a specific species
+class SpeciesParticle
+{
+  long long ID;
+  double x[3];
+  double u[3];
+  double q;
+ public:
+  // accessors
+  long long get_ID()const{ return ID; }
+  double get_x(int i)const{ return x[i]; }
+  double get_u(int i)const{ return u[i]; }
+  double get_q()const{ return q; }
+  void set_ID(long long in){ ID=in; }
+  void set_x(int i, double in) { x[i] = in; }
+  void set_u(int i, double in) { u[i] = in; }
+  void set_q(double in) { q = in; }
+  // alternative accessors
+  double get_x()const{ return x[0]; }
+  double get_y()const{ return x[1]; }
+  double get_z()const{ return x[2]; }
+  double get_u()const{ return u[0]; }
+  double get_v()const{ return u[1]; }
+  double get_w()const{ return u[2]; }
+  void set_x(double in){ x[0]=in; }
+  void set_y(double in){ x[1]=in; }
+  void set_z(double in){ x[2]=in; }
+  void set_u(double in){ u[0]=in; }
+  void set_v(double in){ u[1]=in; }
+  void set_w(double in){ u[2]=in; }
+  void set(long long _ID,
+    double _x, double _y, double _z,
+    double _u, double _v, double _w,
+    double _q)
+  {
+    ID = _ID;
+    x[0] = _x; x[1] = _y; x[2] = _z;
+    u[0] = _u; u[1] = _v; u[2] = _w;
+    q = _q;
+  }
+};
+
+// intended to occupy 64 bytes
+//
+// to be used when sorting with every particle advance
+struct CellParticle
+{
+  long long ID; // 8 bytes
+  int cx[3]; // mesh cell
+  float fx[3]; // mesh cell position (fraction)
+  float u[3];
+  float fxavg[3]; // for implicit push
+  float q; // float m would be better for stitching to MHD for dusty plasma
+  float qom; // for dusty plasma
+ public:
+  // accessors
+  //
+  // read access
+  long long get_ID()const{ return ID; }
+  float get_fx()const{ return fx[0]; }
+  float get_fy()const{ return fx[1]; }
+  float get_fz()const{ return fx[2]; }
+  float get_u()const{ return u[0]; }
+  float get_v()const{ return u[1]; }
+  float get_w()const{ return u[2]; }
+  float get_q()const{ return q; }
+  void set_ID(long long in){ ID=in; }
+  // write access
+  void set_u(float in){ u[0]=in; }
+  void set_v(float in){ u[1]=in; }
+  void set_w(float in){ u[2]=in; }
+
+  void init(const SpeciesParticle& pcl,
+    double cxstart[3], // starting position of cell coordinates
+    float dx_inv[3],
+    float _qom)
+  {
+    ID = pcl.get_ID();
+    // position in mesh coordinates
+    //
+
+    float xpos[3];
+    for(int i=0;i<3;i++)
+    {
+      float xpos = (pcl.get_x(i)-cxstart[i])*dx_inv[i];
+      float cxpos = floor(xpos);
+      cx[i] = int(cxpos);
+      fxavg[i] = fx[i] = cxpos - cx[i];
+      u[i] = pcl.get_u(i);
+    }
+    q = pcl.get_q();
+    qom = _qom;
+  }
+};
+
+#endif

commit b947bb4c39cef810d15c019b9c60f88a2bd1938b

fixed compiler bugs on MIC introduced in e1dba2f4abd

include/Grid3DCU.h
@@ -170,18 +170,18 @@ public: // accessors (inline)
   // coordinate accessors
   //
   // calculated equivalents (preferred for accelerator?):
-  const double calcXN(int X) { return xStart+(X-1)*dx;}
-  const double calcYN(int Y) { return yStart+(Y-1)*dy;}
-  const double calcZN(int Z) { return zStart+(Z-1)*dz;}
-  const pfloat &get_pfloat_XN(int X) { return pfloat_node_xcoord[X];}
-  const pfloat &get_pfloat_YN(int Y) { return pfloat_node_ycoord[Y];}
-  const pfloat &get_pfloat_ZN(int Z) { return pfloat_node_zcoord[Z];}
-  const double &getXN(int X) { return node_xcoord[X];}
-  const double &getYN(int Y) { return node_ycoord[Y];}
-  const double &getZN(int Z) { return node_zcoord[Z];}
-  const double &getXC(int X) { return center_xcoord[X];}
-  const double &getYC(int Y) { return center_ycoord[Y];}
-  const double &getZC(int Z) { return center_zcoord[Z];}
+  double calcXN(int X)const{ return xStart+(X-1)*dx;}
+  double calcYN(int Y)const{ return yStart+(Y-1)*dy;}
+  double calcZN(int Z)const{ return zStart+(Z-1)*dz;}
+  const pfloat &get_pfloat_XN(int X)const{ return pfloat_node_xcoord[X];}
+  const pfloat &get_pfloat_YN(int Y)const{ return pfloat_node_ycoord[Y];}
+  const pfloat &get_pfloat_ZN(int Z)const{ return pfloat_node_zcoord[Z];}
+  const double &getXN(int X)const{ return node_xcoord[X];}
+  const double &getYN(int Y)const{ return node_ycoord[Y];}
+  const double &getZN(int Z)const{ return node_zcoord[Z];}
+  const double &getXC(int X)const{ return center_xcoord[X];}
+  const double &getYC(int Y)const{ return center_ycoord[Y];}
+  const double &getZC(int Z)const{ return center_zcoord[Z];}
   //
   // The following could be eliminated in favor of the previous
   // unless we truly anticipate generalizing to a deformed

include/ipicmath.h
@@ -1,5 +1,6 @@
 #ifndef _ipicmath_h_
 #define _ipicmath_h_
+#include "assert.h"
 
 // valid if roundup power is representable.
 inline int

particles/Particles3Dcomm.cpp
@@ -25,6 +25,7 @@ developers: Stefano Markidis, Giovanni Lapenta.
 #include "ompdefs.h"
 #include "ipicmath.h"
 
+#include "Particle.h"
 #include "Particles3Dcomm.h"
 #include "Parameters.h"
 
@@ -191,7 +192,7 @@ void Particles3Dcomm::allocate(int species, CollectiveIO * col, VirtualTopology3
   pcls = new SpeciesParticle[npmax];
   particleType = ParticleType::SoA;
   #ifdef __INTEL_COMPILER
-    assert_eq(sizeof(Particle),64);
+    assert_eq(sizeof(SpeciesParticle),64);
     ALIGNED(pcls);
   #endif
   //
@@ -1253,8 +1254,8 @@ void Particles3Dcomm::copyParticlesToSoA()
 
 void Particles3Dcomm::copyParticlesToAoS()
 {
-  #pragma omp for
   dprintf("copying to array of structs");
+  #pragma omp for
   for(int pidx=0; pidx<nop; pidx++)
   {
     pcls[pidx].set( ParticleID ? ParticleID[pidx] : 0,

utility/debug.cpp
@@ -70,7 +70,7 @@ void fprintf_fileLine(FILE * fptr,
   const char *type, const char *func, const char *file, int line_number,
   const char *format, ...)
 {
-  //if(!is_output_thread()) return; // temporary
+  if(!is_output_thread()) return; // temporary
 
   // writing directly to fptr would avoid limiting the length
   // of the output string, but by first writing to a string