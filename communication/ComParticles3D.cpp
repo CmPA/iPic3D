091349abc43c8e791f3dfe4afcfbfc4c092ca40f
eajohnson <e.alec.johnson@gmail.com>
2014-03-07 14:35:53 +0100

    moved MPI_Allreduce calls from .h to .cpp

--- communication/ComParticles3D.cpp
+++ communication/ComParticles3D.cpp
@@ -1,5 +1,6 @@
 
 #include "ComParticles3D.h"
+#include "ipicdefs.h"
 
 /** comunicate particles and receive particles to and from 6 processors */
 void communicateParticles(int buffer_size, double *b_Xleft, double *b_Xright, double *b_Yleft, double *b_Yright, double *b_Zleft, double *b_Zright, VirtualTopology3D * vct) {
@@ -10,3 +11,20 @@ void communicateParticles(int buffer_size, double *b_Xleft, double *b_Xright, do
   // DIR Z
   communicateParticlesDIR(buffer_size, vct->getCartesian_rank(), vct->getZright_neighbor_P(), vct->getZleft_neighbor_P(), 2, vct->getXLEN(), vct->getYLEN(), vct->getZLEN(), b_Zright, b_Zleft);
 }
+
+/** communicate the number of particles are not in the right domain*/
+int reduceNumberParticles(int rightDomain) {
+  int result = 0;
+  former_MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Allreduce(&rightDomain, &result, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  return (result);
+}
+
+/** communicate the maximum number of particles from a domain */
+int reduceMaxNpExiting(int npExitingMax) {
+  int result = 0;
+  former_MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Allreduce(&npExitingMax, &result, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+  return (result);
+}
+
--- include/ComParticles3D.h
+++ include/ComParticles3D.h
@@ -9,28 +9,15 @@ developers           : Stefano Markidis, Giovanni Lapenta
 #ifndef ComParticles3D_H
 #define ComParticles3D_H
 
-#include "MPIdata.h"
-#include "ipicdefs.h"
 #include "ComBasic3D.h"
 
 /** comunicate particles and receive particles to and from 6 processors */
 void communicateParticles(int buffer_size, double *b_Xleft, double *b_Xright, double *b_Yleft, double *b_Yright, double *b_Zleft, double *b_Zright, VirtualTopology3D * vct);
 
 /** communicate the number of particles are not in the right domain*/
-inline int reduceNumberParticles(int rightDomain) {
-  int result = 0;
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Allreduce(&rightDomain, &result, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-  return (result);
-}
+int reduceNumberParticles(int rightDomain);
 
 /** communicate the maximum number of particles from a domain */
-inline int reduceMaxNpExiting(int npExitingMax) {
-  int result = 0;
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Allreduce(&npExitingMax, &result, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-  return (result);
-}
-
+int reduceMaxNpExiting(int npExitingMax);
 
 #endif
