0e2904b715133a2c89824473f373efa2d7c87a27
Jorge Amaya <jorgeluis.amaya@gmail.com>
2015-12-24 13:58:37 +0100

    Boundary injection uses LINEAR in Mercury

--- particles/Particles3D.cpp
+++ particles/Particles3D.cpp
@@ -811,7 +811,7 @@ int Particles3D::particle_repopulator(Grid* grid,VirtualTopology3D* vct, Field* 
 
   /* -- NOTE: Hardcoded option -- */
   enum {LINEAR,INITIAL,FFIELD};
-  int rtype = INITIAL;
+  int rtype = LINEAR;
   /* -- END NOTE -- */
 
   if (vct->getCartesian_rank()==0){
