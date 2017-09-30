c9ceacc3094947a778ecbf8acdc278eb02561406
Giovanni Lapenta <valsusa@gmail.com>
2017-09-30 02:42:28 -0600

    modifications to fluxes diangostics

--- matlab/ipic3d_toolbox/binaryVTK/energy_fluxes_GSM_top_div.m
+++ matlab/ipic3d_toolbox/binaryVTK/energy_fluxes_GSM_top_div.m
@@ -68,6 +68,9 @@ ir=bufferX:Nx-bufferX;
 jr=bufferY:Ny-bufferY;
 kr=bufferZ:Nz-bufferZ;
 
+
+jr=round(Ny/2)+[-5:5];
+
 Wm3 = code_E*code_J*4*pi; %4pi is due to the usal division by 4pi of the density
 nWm3 = 1e9*Wm3;
 mWm2= Wm3*code_dp*1e3
@@ -90,11 +93,11 @@ skip=10
 % Compute J dot E
 JedotE=dot(Jex,Jey,Jez,Ex,Ey,Ez);
 method='gaussian'
-radius=7;
-JedotEsm=dot(smooth3(Jex,method,radius),smooth3(Jey,method,radius),smooth3(Jez,method,radius), ...
-    smooth3(Ex,method,radius),smooth3(Ey,method,radius),smooth3(Ez,method,radius));
-JidotEsm=dot(smooth3(Jix,method,radius),smooth3(Jiy,method,radius),smooth3(Jiz,method,radius), ...
-    smooth3(Ex,method,radius),smooth3(Ey,method,radius),smooth3(Ez,method,radius));
+radius=10;
+JedotEsm=dot(smooth3Dlike(Jex,method,radius),smooth3Dlike(Jey,method,radius),smooth3Dlike(Jez,method,radius), ...
+    smooth3Dlike(Ex,method,radius),smooth3Dlike(Ey,method,radius),smooth3Dlike(Ez,method,radius));
+JidotEsm=dot(smooth3Dlike(Jix,method,radius),smooth3Dlike(Jiy,method,radius),smooth3Dlike(Jiz,method,radius), ...
+    smooth3Dlike(Ex,method,radius),smooth3Dlike(Ey,method,radius),smooth3Dlike(Ez,method,radius));
 
 
 JidotE=dot(Jix,Jiy,Jiz,Ex,Ey,Ez);
@@ -103,7 +106,7 @@ JdotE=JedotEsm+JidotEsm;
 
 [Sx, Sy, Sz] = cross_prod(Ex, Ey, Ez, Bx, By, Bz);
 
-divS = compute_div(x,y,z,smooth3(Sx,method,radius),smooth3(Sy,method,radius),smooth3(Sz,method,radius));
+divS = compute_div(x,y,z,smooth3Dlike(Sx,method,radius),smooth3Dlike(Sy,method,radius),smooth3Dlike(Sz,method,radius));
 
 divS = divS*nWm3;
 % the poynting flux is not in W/m^2 that is in SI unit.
@@ -158,13 +161,13 @@ end
 electrons=1
 if(electrons)
     
-Tepar = code_T * Pepar./(-rhoe) /e/1e3;   
-Teperp = code_T * .5*(Peper1+Peper2)./(-rhoe)/e/1e3; 
+Tepar = 3 * code_T * Pepar./(-rhoe) /e/1e3;   
+Teperp = 3 * code_T * .5*(Peper1+Peper2)./(-rhoe)/e/1e3; 
 Te = code_T *(Pexx+Peyy+Pezz)./(-rhoe) /e/1e3;
-Ubulke=.5*(Jex.*Jex+Jey.*Jey+Jez.*Jez)./(-rhoe).^2 *code_T/e/1e3/256;
+Ubulke=.5*(Jex.*Jex+Jey.*Jey+Jez.*Jez)./(-rhoe).^2 *code_T/e/1e3/(-qom_ele);
  
-divQbulke = compute_div(x,y,z,smooth3(Qbulkex,method,radius),smooth3(Qbulkey,method,radius),smooth3(Qbulkez,method,radius));    
-divQenthe = compute_div(x,y,z,smooth3(Qenthex,method,radius),smooth3(Qenthey,method,radius),smooth3(Qenthez,method,radius));
+divQbulke = compute_div(x,y,z,smooth3Dlike(Qbulkex,method,radius),smooth3Dlike(Qbulkey,method,radius),smooth3Dlike(Qbulkez,method,radius));    
+divQenthe = compute_div(x,y,z,smooth3Dlike(Qenthex,method,radius),smooth3Dlike(Qenthey,method,radius),smooth3Dlike(Qenthez,method,radius));
 
 
 labelc = 'nW/m^3';
@@ -191,35 +194,37 @@ newsmooth=1
 if (newsmooth)
 %radius=5
 %method='gaussian';
-Vx=smooth3(Jex./rhoe,method,radius);
-Vy=smooth3(Jey./rhoe,method,radius);
-Vz=smooth3(Jez./rhoe,method,radius);
-tmp = divergence(x,y,z,smooth3(permute(Pexx,[2 1 3]),method,radius), smooth3(permute(Pexy, [2 1 3]),method,radius), smooth3(permute(Pexz, [2,1,3]),method,radius));
+Vx=smooth3Dlike(Jex./rhoe,method,radius);
+Vy=smooth3Dlike(Jey./rhoe,method,radius);
+Vz=smooth3Dlike(Jez./rhoe,method,radius);
+tmp = divergence(x,y,z,smooth3Dlike(permute(Pexx,[2 1 3]),method,radius), smooth3Dlike(permute(Pexy, [2 1 3]),method,radius), smooth3Dlike(permute(Pexz, [2,1,3]),method,radius));
 tmp=permute(tmp,[2 1 3]);
 udivP = tmp.* Vx;
-tmp = divergence(x,y,z,smooth3(permute(Pexy,[2 1 3]),method,radius), smooth3(permute(Peyy, [2 1 3]),method,radius), smooth3(permute(Peyz, [2,1,3]),method,radius));
+tmp = divergence(x,y,z,smooth3Dlike(permute(Pexy,[2 1 3]),method,radius), smooth3Dlike(permute(Peyy, [2 1 3]),method,radius), smooth3Dlike(permute(Peyz, [2,1,3]),method,radius));
 tmp=permute(tmp,[2 1 3]);
 udivP = udivP + tmp.* Vy;
-tmp = divergence(x,y,z,smooth3(permute(Pexz,[2 1 3]),method,radius), smooth3(permute(Peyz, [2 1 3]),method,radius), smooth3(permute(Pezz, [2,1,3]),method,radius));
+tmp = divergence(x,y,z,smooth3Dlike(permute(Pexz,[2 1 3]),method,radius), smooth3Dlike(permute(Peyz, [2 1 3]),method,radius), smooth3Dlike(permute(Pezz, [2,1,3]),method,radius));
 tmp=permute(tmp,[2 1 3]);
 udivP = udivP + tmp.* Vz;
 
 tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),mean(udivP(ir,jr,kr),2)*nWm3,Vex(ir,kr),Vez(ir,kr) ,['UdivPe Z=' 'AVG_Z'],'UdivPe2',[-1 1]*3e-2, Nsm, 7+iz);
 end
+tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),(-mean(divQenthe(ir,jr,kr),2)+mean(UdivPe(ir,jr,kr),2))*nWm3,Vex(ir,kr),Vez(ir,kr) ,['Source Z=' 'AVG_Z'],'Source',[-1 1]*3e-2, Nsm, 7+iz);
+end
 
 end
 
-ions=0
+ions=1
 if(ions)
-divQbulki = compute_div(x,y,z,smooth3(Qbulkix,method,radius),smooth3(Qbulkiy,method,radius),smooth3(Qbulkiz,method,radius));
+divQbulki = compute_div(x,y,z,smooth3Dlike(Qbulkix,method,radius),smooth3Dlike(Qbulkiy,method,radius),smooth3Dlike(Qbulkiz,method,radius));
 
-divQenthi = compute_div(x,y,z,smooth3(Qenthix,method,radius),smooth3(Qenthiy,method,radius),smooth3(Qenthiz,method,radius));
+divQenthi = compute_div(x,y,z,smooth3Dlike(Qenthix,method,radius),smooth3Dlike(Qenthiy,method,radius),smooth3Dlike(Qenthiz,method,radius));
 
 
-Tipar = code_T * Pipar./(rhoi) /e/1e3;   
-Tiperp = code_T * .5*(Piper1+Piper2)./(rhoi)/e/1e3; 
+Tipar = 3 * code_T * Pipar./(rhoi) /e/1e3;   
+Tiperp = 3 *code_T * .5*(Piper1+Piper2)./(rhoi)/e/1e3; 
 Ti = code_T *(Pixx+Piyy+Pizz)./(rhoi) /e/1e3;
-
+Ubulki=.5*(Jix.*Jix+Jiy.*Jiy+Jiz.*Jiz)./(rhoi).^2 *code_T/e/1e3;
 labelc = 'nW/m^3';
 tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),mean(JidotEsm(ir,jr,kr),2)*nWm3,Vix(ir,kr),Viz(ir,kr),['JiE Z=' 'AVG_Z'],'JiE',[-1 1]*0e-10, Nsm,8+iz);
 tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),mean(divQbulki(ir,jr,kr),2)*nWm3,Vix(ir,kr),Viz(ir,kr),['divQbulki Z=' 'AVG_Z'],'divQbulki',[-1 1]*0e-10, Nsm,9+iz);
@@ -231,6 +236,7 @@ labelc = 'keV';
 tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),mean(Tipar(ir,jr,kr),2),Vex(ir,kr),Vez(ir,kr),['Tipar Z=' 'AVG_Z'],'Tipar',[-1 1]*0e-10, Nsm,5+iz);
 tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),mean(Tiperp(ir,jr,kr),2),Vex(ir,kr),Vez(ir,kr),['Tiperp Z=' 'AVG_Z'],'Tiperp',[-1 1]*0e-10, Nsm,5+iz);
 tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),mean(Ti(ir,jr,kr),2),Vex(ir,kr),Vez(ir,kr),['Ti Z=' 'AVG_Z'],'Ti',[-1 1]*0e-10, Nsm,5+iz);
+tmp=common_image_vel(gsmx(X(kr,ir)),gsmz2y(Z(kr,ir)),mean(Ubulki(ir,jr,kr),2),Vex(ir,kr),Vez(ir,kr),['Ubulki Z=' 'AVG_Z'],'Ubulki',[-1 1]*0e-10, Nsm,5+iz);
 
 symmetric_color=1;
 color_choice =3;
