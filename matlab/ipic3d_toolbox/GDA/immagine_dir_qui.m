999649c5aa93e6164ef82310b79b17d5b5936598
valsusa <valsusa@gmail.com>
2017-10-27 10:54:54 +0200

    cleaning matlab

--- matlab/ipic3d_toolbox/GDA/immagine_dir_qui.m
+++ matlab/ipic3d_toolbox/GDA/immagine_dir_qui.m
@@ -1,14 +1,14 @@
-function [J]=immagine_dir(x,y,J1,name,lmt, Nsm, Ncycle, Ygsm, nfig, labelx,labely, labelc)
-global color_choice symmetric_color labelT
+function [J]=immagine_dir_qui(x,y,J1,name,lmt, Nsm, Ncycle, Ygsm, nfig, labelx,labely, labelc)
+global color_choice symmetric_color
 
 load cm_new
 load cm_multi4
 
 J=smooth(J1,Nsm);
 
-h=figure(nfig);
-%set(h,'Position',[167 51 500*max(x)/max(y)*1.2 500])
-Ncut=max(Nsm*3,1);
+h=figure(nfig)
+set(h,'Position',[167 51 500*max(x)/max(y)*1.2 500])
+Ncut=max(Nsm*3,1)
 
 if(lmt(1) == lmt(2))
 lmt(1)=min(J(:));
@@ -25,14 +25,13 @@ lmt(1)=-lmt(2)
 elseif (symmetric_color==-2)
 lmt(2)=(mean(abs(J(:))) + max(abs(J(:))))/2;
 lmt(1)=0.0
+elseif (symmetric_color==-3)
+lmt(2)=sqrt(mean(J(:).^2));
+lmt(1)=0.0
 end
 end
 
-ax=imagesc(x,y,J(Ncut:end-Ncut,Ncut:end-Ncut)',lmt);
-%[nx,ny]=size(J(Ncut:end-Ncut,Ncut:end-Ncut)')
-%v=linspace(min(lmt),max(lmt),20)
-%ax=contourf(linspace(min(y),max(y),ny),linspace(min(x),max(x),nx),J(Ncut:end-Ncut,Ncut:end-Ncut)',v)
-
+imagesc(x,y,J(Ncut:end-Ncut,Ncut:end-Ncut)',lmt)
 
         if(color_choice==0)
         colormap jet
@@ -42,35 +41,20 @@ ax=imagesc(x,y,J(Ncut:end-Ncut,Ncut:end-Ncut)',lmt);
         colormap(cool)
         elseif (color_choice==3)
         colormap(cm_cool_hot_2)
-        elseif (color_choice==4)
-           colormap('default')
         end
         
         
-        c = colorbar;
+        c = colorbar
         ylabel(c,labelc)
         set(gca,'fontsize',[14])
 set(gca,'fontsize',[14])
 xlabel(labelx,'fontsize',[14])
 ylabel(labely,'fontsize',[14])
 
-title([labelT Ncycle '  Npart=' num2str(Ygsm)], 'fontsize',[14])
-title([labelT Ncycle ], 'fontsize',[14])
-
+title(['\omega_{ci}t= ' Ncycle], 'fontsize',[14])
         
-%axis image
+axis image
 axis xy
-axis square
-
-
-
-grid on
-ax=gca;
-ax.GridColor='w';
-ax.GridAlpha=1;
-ax.YTick=ax.XTick;
-%ax.LineWidth=1;
-
 %set(gca,'xdir','reverse','TickDir','out')
 %set(gca,'TickDir','out')
 %print('-depsc','-r300',[name '.eps'])
