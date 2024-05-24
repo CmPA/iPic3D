6cbbb7e0286026240ba9b50a242c96183ed55a2b
PJD <pranab.deka@kuleuven.be>
2024-05-24 19:39:31 +0200

    Including vtx for OpenACC

--- cmake-modules/FindPETSc.cmake
+++ cmake-modules/FindPETSc.cmake
@@ -274,7 +274,7 @@ int main(int argc,char *argv[]) {
   find_path (PETSC_INCLUDE_CONF petscconf.h HINTS "${PETSC_DIR}" PATH_SUFFIXES "${PETSC_ARCH}/include" "bmake/${PETSC_ARCH}" NO_DEFAULT_PATH)
   mark_as_advanced (PETSC_INCLUDE_DIR PETSC_INCLUDE_CONF)
   set (petsc_includes_minimal ${PETSC_INCLUDE_CONF} ${PETSC_INCLUDE_DIR})
-#[[
+
   petsc_test_runs ("${petsc_includes_minimal}" "${PETSC_LIBRARIES_TS}" petsc_works_minimal)
   if (petsc_works_minimal)
     message (STATUS "Minimal PETSc includes and libraries work.  This probably means we are building with shared libs.")
@@ -305,7 +305,7 @@ int main(int argc,char *argv[]) {
       endif (petsc_works_alllibraries)
     endif (petsc_works_allincludes)
   endif (petsc_works_minimal)
-#]]
+
   # We do an out-of-source build so __FILE__ will be an absolute path, hence __INSDIR__ is superfluous
   if (${PETSC_VERSION} VERSION_LESS 3.1)
     set (PETSC_DEFINITIONS "-D__SDIR__=\"\"" CACHE STRING "PETSc definitions" FORCE)
--- iPic3D.cpp
+++ iPic3D.cpp
@@ -2,94 +2,128 @@
 #include <iomanip>
 #include "iPic3D.h"
 #include "MyClock.h"
+#include "nvtx.h"
 
 MyClock *clocks;
 
 using namespace iPic3D;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 
-  iPic3D::c_Solver KCode;
-  bool b_err = false;
+	iPic3D::c_Solver KCode;
+	bool b_err = false;
 
-  /* ------------------------------ */
-  /* 0- Initialize the solver class */
-  /* ------------------------------ */
+	//! ------------------------------ !//
+	//! 0- Initialize the solver class !//
+	//! ------------------------------ !//
 
-  KCode.Init(argc, argv);
-  KCode.InjectBoundaryParticles();
-  KCode.GatherMoments();
+	KCode.Init(argc, argv);
+	KCode.InjectBoundaryParticles();
+	KCode.GatherMoments();
 
-  /* ------------ */
-  /* 1- Main loop */
-  /* ------------ */
+	//! ------------ !//
+	//! 1- Main loop !//
+	//! ------------ !//
+  	for (int i = KCode.FirstCycle(); i <= KCode.LastCycle(); i++)
+	{
 
-  for (int i = KCode.FirstCycle(); i <= KCode.LastCycle(); i++) {
+		if (KCode.get_myrank() == 0) cout << " ======= Cycle " << i << " ======= " << endl;
 
-    if (KCode.get_myrank() == 0) cout << " ======= Cycle " << i << " ======= " << endl;
+		//! ----------------------------------------------------- !//
+		//! 2- Calculate fields and move particles                !//
+		//!    Exit if there is a memory issue with the particles !//
+		//! ----------------------------------------------------- !//
 
-    /* ----------------------------------------------------- */
-    /* 2- Calculate fields and move particles                */
-    /*    Exit if there is a memory issue with the particles */
-    /* ----------------------------------------------------- */
+		KCode.UpdateCycleInfo(i);
 
-    KCode.UpdateCycleInfo(i);
+		//* ------------------------------------------------------ *//
 
-    clocks->start(2);
-    KCode.CalculateField();
-    clocks->stop(2);
+		#ifdef NSIGHT_PROFILING
+			mynvtxstart_("Field_Solver");
+		#endif
+		clocks->start(2);
+		KCode.CalculateField();
+		clocks->stop(2);
+		#ifdef NSIGHT_PROFILING
+			mynvtxstop_();
+		#endif
 
-    clocks->start(3);
-    b_err = KCode.ParticlesMover();
+		//* ------------------------------------------------------ *//
 
+		#ifdef NSIGHT_PROFILING
+			mynvtxstart_("Particle_Mover");
+		#endif
+		clocks->start(3);
+		b_err = KCode.ParticlesMover();
+		if (!b_err) KCode.CalculateBField();
+		clocks->stop(3);
+		#ifdef NSIGHT_PROFILING
+			mynvtxstop_();
+		#endif
 
-    if (!b_err) KCode.CalculateBField();
-    clocks->stop(3);
-    clocks->start(1);
-    if (!b_err) KCode.GatherMoments();
-    clocks->stop(1);
-    if ( b_err) i = KCode.LastCycle() + 1;
+		//* ------------------------------------------------------ *//
 
-    /* --------------- */
-    /* 3- Output files */
-    /* --------------- */
+		#ifdef NSIGHT_PROFILING
+			mynvtxstart_("Moment_Gathering");
+		#endif
+		clocks->start(1);
+		if (!b_err) KCode.GatherMoments();
+		clocks->stop(1);
+		if ( b_err) i = KCode.LastCycle() + 1;
 
-    clocks->start(4);
-    KCode.WriteOutput(i);
-    KCode.WriteConserved(i);
-    KCode.WriteRestart(i);
-    clocks->stop(4);
+		//* ------------------------------------------------------ *//
 
-    if (i == 0 || i%(10)==0) {
+		//! --------------- !//
+		//! 3- Output files !//
+		//! --------------- !//
 
-        	if (KCode.get_myrank() == 0) {
-        	    std::cout << "################################################" << std::endl
-        	            << "Initialization                : " << clocks->get(0) << " s" << std::endl
-        	            << "Moments Gathering             : " << clocks->get(1) << " s" << std::endl
-        	            << "Field Calculation             : " << clocks->get(2) << " s" << std::endl
-        	            << "Particle Mover                : " << clocks->get(3) << " s" << std::endl
-        	            << "Writing                       : " << clocks->get(4) << " s" << std::endl
-        	            << "**************************************************" << std::endl;
-        	  }
-      }
+		#ifdef NSIGHT_PROFILING    
+			mynvtxstart_("Write_data");
+		#endif 
+		clocks->start(4);
+		KCode.WriteOutput(i);
+		KCode.WriteConserved(i);
+		// KCode.WriteRestart(i);
+		clocks->stop(4);
+		#ifdef NSIGHT_PROFILING    
+			mynvtxstop_();
+		#endif
 
-  }
+		if (i == 0 || i%(10)==0) 
+		{
+			if (KCode.get_myrank() == 0) 
+			{
+				std::cout << "################################################" << std::endl
+						<< "Initialization                : " << clocks->get(0) << " s" << std::endl
+						<< "Moments Gathering             : " << clocks->get(1) << " s" << std::endl
+						<< "Field Calculation             : " << clocks->get(2) << " s" << std::endl
+						<< "Particle Mover                : " << clocks->get(3) << " s" << std::endl
+						<< "Writing                       : " << clocks->get(4) << " s" << std::endl
+						<< "**************************************************" << std::endl;
+			}
 
-int myrank;
-myrank = KCode.get_myrank();
-//MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-  KCode.Finalize();
+		} 		//? end if (i == 0 || i%(10)==0) 
 
-  if (myrank == 0) {
-          	    std::cout << "################################################" << std::endl
-          	            << "Initialization                : " << clocks->get(0) << " s" << std::endl
-          	            << "Moments Gathering             : " << clocks->get(1) << " s" << std::endl
-          	            << "Field Calculation             : " << clocks->get(2) << " s" << std::endl
-          	            << "Particle Mover                : " << clocks->get(3) << " s" << std::endl
-          	            << "Writing                       : " << clocks->get(4) << " s" << std::endl
-          	            << "----------------------------------------------------------" << std::endl
-          	            << "Total                         : " << clocks->get(5)<< " s" << std::endl;
-          	  }
+	}			//? end for (int i = KCode.FirstCycle(); i <= KCode.LastCycle(); i++)
 
-  return 0;
-}
+	int myrank;
+	myrank = KCode.get_myrank();
+	//MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	KCode.Finalize();
+
+	if (myrank == 0) 
+  	{
+		std::cout << "################################################" << std::endl
+				<< "Initialization                : " << clocks->get(0) << " s" << std::endl
+				<< "Moments Gathering             : " << clocks->get(1) << " s" << std::endl
+				<< "Field Calculation             : " << clocks->get(2) << " s" << std::endl
+				<< "Particle Mover                : " << clocks->get(3) << " s" << std::endl
+				<< "Writing                       : " << clocks->get(4) << " s" << std::endl
+				<< "----------------------------------------------------------" << std::endl
+				<< "Total                         : " << clocks->get(5)<< " s" << std::endl;
+	}
+
+	return 0;
+
+}				//? end int main(int argc, char **argv) 
